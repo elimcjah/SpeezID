38b7fa43fd33b185c3529c0f340bbb1e6b0e0334
Elijah McClendon <Elijah@fungry.com>
2017-04-21 21:07:48 -0600

    Push to heroku again.

--- app.js
+++ app.js
@@ -26,8 +26,6 @@ require('dotenv').config();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.set('model/', path.join(__dirname, 'model'));
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
